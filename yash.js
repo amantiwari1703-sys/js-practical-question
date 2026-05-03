078456c51da19a0c2b6283c77e58eb216706b9e1
Aman Tiwari <amantiwari@gmail.com>
2026-05-03 20:51:28 +0530

    merge

--- yash.js
+++ yash.js
@@ -1,51 +1,11 @@
-for(var i=1;i<=10;i++){
-    console.log(i);
-}
-var arr=[10,20,30,40];
-// console.log(arr);
-for(var a=0;a<arr.length;a++){
-    console.log(arr[a]);
-    
-}
-var names=["Ram","Shyam","Mohan"];
-for(var b=0;b<names.length;b++){
-    console.log(names[b]);
-    
-}
-var str="Hello";
-for(var c=0;c<str.length;c++){
-    console.log(str[c]);
-    
-}
 let nums=[10,20,30];
-console.log(nums.length-1);
-let no=[10,20];
-no.push(30);
-console.log(no);
-let sum=[10,20,30];
-sum.pop();
-console.log(sum);
-let sums=[20,30];
-sums.unshift(10);
-console.log(sums);
-let num=[10,20,30];
-num.shift();
-console.log(num);
-let digit=[10,20,30];
-digit[1]=35;
-console.log(digit);
-let digits=[10,30];
-digits.splice(1,0,20);
-console.log(digits);
+let copy =[...nums];
+console.log(copy);
+let a=[1,2];
+let b=[3,4];
+let merged =[...a,...b];
+console.log(merged);
 
-let NUM=[10,20,30];
-NUM.splice(1,1);
-console.log(NUM);
-let DIGIT =[10,20,30,40];
-console.log(DIGIT.includes(20));
-
-let NO=[10,20,30];
-console.log(NO.indexOf(30));
 
 
 
