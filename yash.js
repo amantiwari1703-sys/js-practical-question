adc31444f3db73957c097a16e43848d2476745a5
Aman Tiwari <amantiwari@gmail.com>
2026-05-03 22:02:01 +0530

    else

--- yash.js
+++ yash.js
@@ -1,20 +1,30 @@
-var num=parseInt(prompt("enter the num"));
+// var num=parseInt(prompt("enter the num"));
 
-switch(num){
-    case 1 :
-        console.log("case one");
-        break;
+// switch(num){
+//     case 1 :
+//         console.log("case one");
+//         break;
         
-    case 2:
-        console.log("case two");
-    case 2:
-        console.log("case three");
-}    
+//     case 2:
+//         console.log("case two");
+//     case 2:
+//         console.log("case three");
+// }    
 
 
 
 
-
+let marks=parseInt(prompt("enter the marks"))
+if(marks>90){
+    console.log("topper");
+    
+}else if(marks>70){
+    console.log("excellant");
+    
+}else{
+    console.log("poor");
+    
+}
 
 
 
